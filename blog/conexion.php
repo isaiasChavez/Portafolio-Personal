acba30131e56c1e0157505dbd64a8df4691d1eb1
isaias <isaias.ch4vez@gmail.com>
2019-09-24 10:50:51 -0500

    Se empezó a hacer la petición de datos por medio de js a php pero ocurrió un error al parsear los datos, aún no se corrige

--- blog/conexion.php
+++ blog/conexion.php
@@ -4,7 +4,6 @@ try {
     $contraseña = '';
 
     $pdo = new PDO('mysql:host=localhost;dbname=blog', $usuario, $contraseña);
-
 } catch (PDOException $e) {
     print "¡Error!: " . $e->getMessage() . "<br/>";
     die();
--- blog/script.js
+++ blog/script.js
@@ -7,3 +7,61 @@ barmenu.classList.toggle("mostrarMenu")
 })
 
 
+
+
+class Blog{
+    constructor(){
+        this.listarMenuArticulos = this.listarMenuArticulos.bind(this)
+        this.numeroDeArticulos = 0;
+        this.inicializar()
+    }
+    inicializar(){
+        
+
+        this.$contenedorMenuDeArticulos = document.getElementById('post-list')
+        this.$itemMenuArticulos =  `
+        <a href=""> <li>contenido</li></a>
+        `;
+        this.traerDatos()
+        .then(this.listarMenuArticulos)
+        
+
+
+    }
+    traerDatos(){
+        return new Promise((resolve,reject) => {
+            fetch('index.php')
+            .then( response =>{
+                return response.json()
+            })
+            .then( data => {
+                this.listadeArticulos = data
+                resolve()
+            })
+            .catch( error => reject(error))
+
+        })
+        
+    }
+    listarMenuArticulos(){
+        this.limpiarSeccion(this.$contenedorMenuDeArticulos);
+        for (const i in this.listadeArticulos) {
+                const element = this.listadeArticulos[i];
+                console.log(element)
+                this.agregarSeccion(this.$contenedorMenuDeArticulos,this.$itemMenuArticulos.replace('contenido',element.titulo))
+        }
+    }
+
+    limpiarSeccion(contenedor){
+        contenedor.innerHTML = '';
+    }
+    agregarSeccion(contenedor,articulo){
+        contenedor.innerHTML += articulo;
+    }
+    
+
+
+
+}
+
+window.Blog = new Blog();
